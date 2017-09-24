ced1f014768b2944ebb2ec9a45f93f3f179d73cb
Dominic Meiser <dmeiser79@gmail.com>
2017-09-23 18:51:32 -0600

    Add create_ensemble to ion_trapping module.

--- ion_trapping/__init__.py
+++ ion_trapping/__init__.py
@@ -1,4 +1,5 @@
 from .ion_trapping import (
     angular_damping,
-    axial_damping
+    axial_damping,
+    create_ensemble
 )
--- ion_trapping/ion_trapping.pyx
+++ ion_trapping/ion_trapping.pyx
@@ -2,6 +2,7 @@ import cython
 import numpy as np
 cimport numpy as np
 cimport ion_trapping_lib
+import coldatoms
 
 
 @cython.boundscheck(False)
@@ -39,3 +40,29 @@ def axial_damping(kappa_dt,
         kappa_dt,
         &v[0, 0]
     )
+
+def create_ensemble(uE, omega_z, mass, charge):
+    num_ions = int(uE.size / 2)
+    x = uE[:num_ions]
+    y = uE[num_ions:]
+    r = np.sqrt(x**2 + y**2)
+    r_hat = np.transpose(np.array([x / r, y / r]))
+    phi_hat = np.transpose(np.array([-y / r, x / r]))
+    v = np.zeros([num_ions, 2], dtype=np.float64)
+    for i in range(num_ions):
+        v[i, 0] = omega_z * r[i] * phi_hat[i, 0]
+        v[i, 1] = omega_z * r[i] * phi_hat[i, 1]
+
+    ensemble = coldatoms.Ensemble(num_ions)
+    for i in range(num_ions):
+        ensemble.x[i, 0] = x[i]
+        ensemble.x[i, 1] = y[i]
+        ensemble.x[i, 2] = 0.0
+        ensemble.v[i, 0] = v[i, 0]
+        ensemble.v[i, 1] = v[i, 1]
+        ensemble.v[i, 2] = 0.0
+
+    ensemble.ensemble_properties['mass'] = mass
+    ensemble.ensemble_properties['charge'] = charge
+
+    return ensemble
