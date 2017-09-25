63903efb1ed192db4dc1e06d89b17970eedb2beb
Dominic Meiser <dmeiser79@gmail.com>
2017-09-25 05:26:49 -0600

    Fixes in fig_confergence_top_view.py.

--- cooling/scripts/fig_confergence_top_view.py
+++ cooling/scripts/fig_confergence_top_view.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 
 
 ensemble = coldatoms.json_to_ensemble(open('initial_state_180kHz.txt').read())
+ref_solution = coldatoms.json_to_ensemble(open('reference_solution_180kHz.txt').read())
 plt.clf()
 visualization.top_view(ensemble.x[:, 0], ensemble.x[:, 1])
+visualization.top_view(ref_solution.x[:, 0], ref_solution.x[:, 1], 'ro')
 plt.savefig('initial_state_top_view.pdf')
 
