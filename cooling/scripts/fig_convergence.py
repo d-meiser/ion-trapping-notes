9daa395c3c9af3aaf91a6a98923b780e03550db2
Dominic Meiser <dmeiser79@gmail.com>
2017-09-25 21:11:35 -0600

    First stab at convergence plot.

--- cooling/scripts/fig_convergence.py
+++ cooling/scripts/fig_convergence.py
@@ -1,8 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 import mode_analysis_code
 import coldatoms
+
+# Enable LaTeX
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+params = {
+    'text.latex.preamble':
+    [r'\usepackage{siunitx}', r'\usepackage{amsmath}'],
+    'xtick.labelsize': 'medium',
+    'ytick.labelsize': 'medium',
+    'axes.labelsize': 'medium'
+}
+plt.rcParams.update(params)
+
+
+line_width = 246.0 / 72.0
+default_width = 0.95 * line_width 
+golden_ratio = 1.61803398875
+default_height = default_width / golden_ratio
 
 
 # Use specific seed to make numpy random number generation reproducible
@@ -57,19 +76,22 @@ def evolve_ensemble(dt, t_max, ensemble, Bz, forces):
     coldatoms.bend_kick(t_max - dt * num_steps, Bz, ensemble, forces)
 
 
-delta_x = 1.0e-7
-delta_v = 1.0e-4
-
 initial_state = coldatoms.json_to_ensemble(open('initial_state_180kHz.txt').read())
-initial_state.x += np.random.normal(loc=0.0, scale=delta_x, size=initial_state.x.shape)
-initial_state.v += np.random.normal(loc=0.0, scale=delta_v, size=initial_state.v.shape)
 
 
 t_max = 1.0e-5
-dt_ref = 5.0e-10
+dt_ref = 2.0e-10
+
 ref_solution = initial_state.copy()
 trap_potential.reset_phase()
+
+t0 = time.clock()
 evolve_ensemble(dt_ref, t_max, ref_solution, mode_analysis.B, forces)
+t1 = time.clock()
+t_elapsed = t1 - t0
+print('total elapsed time: ', t_elapsed)
+print('time per iter:      ', t_elapsed / (t_max / dt_ref))
+
 f = open('reference_solution_180kHz.txt', 'w')
 f.write(coldatoms.ensemble_to_json(ref_solution))
 f.close()
@@ -90,24 +112,36 @@ def mean_error(x, y):
     return error
 
 
-def compute_error(dt, ref_sol):
+def compute_error(dt, initial_state, ref_sol):
     trap_potential.reset_phase()
     my_ensemble = initial_state.copy()
     evolve_ensemble(dt, t_max, my_ensemble, mode_analysis.B, forces)
-    return mean_error(ref_sol.x, my_ensemble.x)
+    # return mean_error(ref_sol.x, my_ensemble.x)
+    return np.linalg.norm(ref_sol.x - my_ensemble.x)
 
 
 dt_min = 1.0e-9
-dt_max = 1.0e-7
+dt_max = 1.0e-5
 num_pts = 100
 ddt = (dt_max / dt_min) ** (1.0 / (num_pts - 1.0))
-dts = [dt_min * ddt**e for e in range(num_pts)]
+dts = np.array([dt_min * ddt**e for e in range(num_pts)])
 
 
-errors = [compute_error(dt, ref_solution) for dt in dts]
+errors = [compute_error(dt, initial_state, ref_solution) for dt in dts]
 
 
 plt.clf()
 plt.loglog(dts, errors)
-plt.savefig('fig_errors.pdf')
+
+quadratic_dts = dts[15:42]
+quadratic = quadratic_dts**2 * (8.0e-9 / dts[0]**2)
+plt.loglog(quadratic_dts, quadratic, '--')
+
+plt.xlabel(r'$\Delta t /\si{s}$')
+plt.ylabel(r'$\Delta x /\si{m}$')
+plt.xlim([0.8e-9, 1.2e-5])
+plt.ylim([1.0e-8, 1.1e1])
+plt.gcf().set_size_inches([default_width, default_height])
+plt.subplots_adjust(left=0.18, right=0.97, top=0.96, bottom=0.21)
+plt.savefig('fig_convergence.pdf')
 
