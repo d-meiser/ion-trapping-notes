d3bb9f68f37e7cb0769b9a7e97bc403b852b4a81
Dominic Meiser <dmeiser79@gmail.com>
2018-05-18 16:35:43 -0600

    Set up simplistic simulation of heating of ion crytal

--- cooling/scripts/fig_heating.py
+++ cooling/scripts/fig_heating.py
@@ -2,18 +2,8 @@ from common import *
 import ion_trapping
 
 
-dt = 1.0e-9
-t_max = 1.0e-7
-my_ensemble = initial_state.copy()
-trap_potential.reset_phase()
-evolve_ensemble(dt, t_max, my_ensemble, mode_analysis.B,
-                forces + [in_plane_cooling] + axial_cooling)
-
-
-def compute_energy(ensemble, B_z, omega, theta, kx, ky, kz):
-    energy = ion_trapping.kinetic_energy(
-            ensemble.x, ensemble.v,
-            ensemble.ensemble_properties['mass'], omega)
+def potential_energy(ensemble, B_z, omega, theta, kx, ky, kz):
+    energy = 0
     energy += ion_trapping.trap_energy(
             ensemble.x,
             kx, ky, kz, theta,
@@ -25,23 +15,50 @@ def compute_energy(ensemble, B_z, omega, theta, kx, ky, kz):
             ensemble.ensemble_properties['charge'])
     return energy
 
-# TODO: Generate a zero temperature initial state using the mode_analysis code.
-my_ensemble.v[:, 0] = -mode_analysis.wrot * my_ensemble.x[:, 1]
-my_ensemble.v[:, 1] =  mode_analysis.wrot * my_ensemble.x[:, 0]
-my_ensemble.v[:, 2] *= 0
 
-print(compute_energy(my_ensemble,
-    mode_analysis.B, mode_analysis.wrot,
-    trap_potential.phi,
-    trap_potential.kx,
-    trap_potential.ky,
-    trap_potential.kz))
-print(ion_trapping.kinetic_energy(
-    my_ensemble.x, my_ensemble.v,
-    my_ensemble.ensemble_properties['mass'],
-    -mode_analysis.wrot))
+def kinetic_energy(ensemble, omega):
+    energy = ion_trapping.kinetic_energy(
+            ensemble.x, ensemble.v,
+            ensemble.ensemble_properties['mass'], omega)
+    return energy
+
+
+def total_energy(ensemble, B_z, omega, theta, kx, ky, kz):
+    energy = 0
+    energy += potential_energy(ensemble, B_z, omega, theta, kx, ky, kz)
+    energy += kinetic_energy(ensemble, omega)
+    return energy
+
+
+dt = 1.0e-9
+t_max = 1.0e-8
+num_steps = 10
+my_ensemble = initial_state.copy()
+trap_potential.reset_phase()
+
+
+kin = [kinetic_energy(my_ensemble, mode_analysis.wrot)]
+pot = [potential_energy(my_ensemble,
+        mode_analysis.B, mode_analysis.wrot,
+        trap_potential.phi,
+        trap_potential.kx,
+        trap_potential.ky,
+        trap_potential.kz)]
+t = 0.0
+for i in range(num_steps):
+    print(i, ' ', t, kin[-1], pot[-1], kin[-1] + pot[-1])
+    evolve_ensemble(dt, t_max, my_ensemble, mode_analysis.B,
+                    forces + [in_plane_cooling] + axial_cooling)
+    t += t_max
+    kin.append(kinetic_energy(my_ensemble, mode_analysis.wrot))
+    pot.append(potential_energy(my_ensemble,
+            mode_analysis.B, mode_analysis.wrot,
+            trap_potential.phi,
+            trap_potential.kx,
+            trap_potential.ky,
+            trap_potential.kz))
+print(num_steps, ' ', t, kin[-1], pot[-1], kin[-1] + pot[-1])
+print(kin)
+
             
-            
-#TODO:
-# - measure temperature
-# - create a plot
+           
