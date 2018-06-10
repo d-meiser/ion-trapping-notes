3e3e1f185ea705844680dc811581aa1bcbafcef1
Dominic Meiser <dmeiser79@gmail.com>
2018-06-10 14:11:29 -0600

    Separate into in-plane and out-of-plane temperature

--- cooling/scripts/fig_heating.py
+++ cooling/scripts/fig_heating.py
@@ -23,6 +23,20 @@ def kinetic_energy(ensemble, omega):
     return energy
 
 
+def kinetic_energy_in_plane(ensemble, omega):
+    energy = ion_trapping.kinetic_energy_in_plane(
+            ensemble.x, ensemble.v,
+            ensemble.ensemble_properties['mass'], omega)
+    return energy
+
+
+def kinetic_energy_out_of_plane(ensemble):
+    energy = ion_trapping.kinetic_energy_out_of_plane(
+            ensemble.x, ensemble.v,
+            ensemble.ensemble_properties['mass'])
+    return energy
+
+
 def total_energy(ensemble, B_z, omega, theta, kx, ky, kz):
     energy = 0
     energy += potential_energy(ensemble, B_z, omega, theta, kx, ky, kz)
@@ -37,31 +51,21 @@ my_ensemble = initial_state.copy()
 trap_potential.reset_phase()
 
 
-kin = [kinetic_energy(my_ensemble, mode_analysis.wrot)]
-pot = [potential_energy(my_ensemble,
-        mode_analysis.B, mode_analysis.wrot,
-        trap_potential.phi,
-        trap_potential.kx,
-        trap_potential.ky,
-        trap_potential.kz)]
+kin_in_plane = [kinetic_energy_in_plane(my_ensemble, mode_analysis.wrot)]
+kin_out_of_plane = [kinetic_energy_out_of_plane(my_ensemble)]
 t = 0.0
-print(' step      t        Ekin         Epot         Etot      Temperature/mK')
-format_string = '%5d  %.2e  %.5e  %.5e  %.5e    %.5e'
+print(' step      t        T_in_plane/mK   T_out_of_plane/mK   T_tot/mK')
+format_string = '%5d  %.2e      %.5e       %.5e       %.5e'
 for i in range(num_steps):
+    t_in_plane = kin_in_plane[-1] / (num_ions * (2.0 / 2.0) * kB * 1.0e-3)
+    t_out_of_plane = kin_out_of_plane[-1] / (num_ions * (1.0 / 2.0) * kB * 1.0e-3)
+    t_tot = (kin_in_plane[-1] + kin_out_of_plane[-1]) / (num_ions * (3.0 / 2.0) * kB * 1.0e-3)
     print(format_string%
-            (i, t, kin[-1], pot[-1], kin[-1] + pot[-1],
-                kin[-1] / num_ions / 1.5 / kB))
+            (i, t, t_in_plane, t_out_of_plane, t_tot))
     evolve_ensemble(dt, t_max, my_ensemble, mode_analysis.B,
                     forces + [in_plane_cooling] + axial_cooling)
     t += t_max
-    kin.append(kinetic_energy(my_ensemble, mode_analysis.wrot))
-    pot.append(potential_energy(my_ensemble,
-            mode_analysis.B, mode_analysis.wrot,
-            trap_potential.phi,
-            trap_potential.kx,
-            trap_potential.ky,
-            trap_potential.kz))
+    kin_in_plane.append(kinetic_energy_in_plane(my_ensemble, mode_analysis.wrot))
+    kin_out_of_plane.append(kinetic_energy_out_of_plane(my_ensemble))
 
-print(format_string %
-        (num_steps, t, kin[-1], pot[-1], kin[-1] + pot[-1],
-            kin[-1] / num_ions / 1.5 / kB))
+
