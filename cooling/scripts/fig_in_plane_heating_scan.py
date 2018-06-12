7db1c211ed6059461eb2597341ccf95e8afb4718
Dominic Meiser <dmeiser79@gmail.com>
2018-06-11 19:17:59 -0600

    Add results from a heating run 3

--- cooling/scripts/fig_in_plane_heating_scan.py
+++ cooling/scripts/fig_in_plane_heating_scan.py
@@ -1,23 +1,23 @@
 import numpy as np
 
 
-def read_data(i, j):
-    filename = 'heating_run_' + str(i) + '_' + str(j) + '.dat'
+def read_data(basename, i, j):
+    filename = basename + '_' + str(i) + '_' + str(j) + '.dat'
     return np.loadtxt(filename, dtype=np.float64, skiprows=2)
 
 for j in range(9):
-    data = read_data(0, j)
+    data = read_data('heating_run', 0, j)
     print(np.mean(data[50:, 2]))
 
 print('\n\n')
 
 for j in range(0, 20):
-    data = read_data(1, j)
+    data = read_data('heating_run', 1, j)
     print(np.mean(data[50:, 2]))
 
 print('\n\n')
 
 for j in range(0, 20):
-    data = read_data(2, j)
+    data = read_data('heating_run', 2, j)
     print(np.mean(data[50:, 2]))
 
